070b0aa9b0f022ad1eee0b3bef9b382262bf2824
Beni <beninoje@hotmail.com>
2024-01-28 20:13:41 -0500

    added header component using react icons and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
