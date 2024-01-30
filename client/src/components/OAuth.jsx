4ec7ad943b528b25b8b67422109391181c085378
Beni <beninoje@hotmail.com>
2024-01-30 16:40:55 -0500

    added Google OAuth backend using FireBase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,31 +1,45 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { app } from '../firebase.js';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/user.slice.js';
+import { Link, useNavigate } from "react-router-dom"
 
 const OAuth = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
-        try{
-           const provider = new GoogleAuthProvider();
-           const auth = getAuth(app);
-
-           const result = await signInWithPopup(auth, provider);
-           const res = await fetch('/api/auth/google',{
-            hethod:'POST',
-            headers:{
+        try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app);
+      
+            const result = await signInWithPopup(auth, provider);
+      
+            const res = await fetch('/api/auth/google', {
+              method: 'POST',
+              headers: {
                 'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL})
-           });
-           const data = await res.json();
-           dispatch(signInSuccess(data))
-        }catch(error){
-            console.log(error);
-        }
-    }
+              },
+              body: JSON.stringify({
+                name: result.user.displayName,
+                email: result.user.email,
+                photo: result.user.photoURL,
+              }),
+            });
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+            navigate('/');
+          } catch (error) {
+            console.log('could not sign in with google', error);
+          }
+        };
   return (
-    <button onClick={handleGoogleClick} type="button" className='bg-red-600 text-white p-3 rounded-lg uppercase'>Continue with google</button>
+    <button 
+        onClick={handleGoogleClick} 
+        type="button" 
+        className='bg-red-600 text-white p-3 rounded-lg uppercase'
+        >
+            Continue with google
+    </button>
   )
 }
 
