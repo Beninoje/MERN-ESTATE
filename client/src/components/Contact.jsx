aa8df6902fd5653a0089ac652927bf0ec46fd6b1
Beni <beninoje@hotmail.com>
2024-02-28 17:16:06 -0500

    completed listing page

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
