5169331dc324b83ceb4dc17a1e0b66bafbe744f9
Beni <beninoje@hotmail.com>
2024-03-02 14:33:11 -0500

    front end changes in homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className=''>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -30,21 +30,6 @@ const Header = () => {
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
-        >
-          <input
-            type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e)=>setSearchTerm(e.target.value)}
-          />
-          <button>
-            <FaSearch className='text-slate-600' />
-          </button>
-        </form>
         <ul className='flex gap-4'>
           <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,36 @@
-import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
-import image1 from '../images/image1.jpg';
-import image2 from '../images/image2.jpg';
-import image3 from '../images/image3.jpg';
-import image4 from '../images/image4.jpg';
-import image5 from '../images/image5.jpg';
+import homeImage from '../images/homeImage.jpg';
+import { FaSearch, FaMoneyBillAlt, FaRegBuilding } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 const Home = () => {
-  const homeImages = [image1, image2, image3, image4, image5]
   const navigate = useNavigate();
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   useEffect(()=>{
     const fetchOfferListings = async() =>{
       try {
@@ -52,26 +66,37 @@ const Home = () => {
   return (
     <div>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
-          <br />
-          place with ease
-        </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
-          <br />
-          We have a wide range of properties for you to choose from.
+      <div className="flex flex-col lg:flex-row lg:gap-[40px] justify-between items-center container mx-auto pt-[100px] pb-[50px] ">
+        <div className="order-2 lg:order-1">
+            <h1 className='title-color text-6xl font-bold'>Discover Most Suitable Property</h1>
+            <p className='desc-color text-xl py-[30px]'>Find a variety of properties that suit you very easily <br></br>& forget all difficulties in finding a residence for you</p>
+            <form
+              onSubmit={handleSubmit}
+              className='rounded-lg flex items-center input-border-stroke w-[600px] p-3'
+            >
+            <FaSearch className='text-slate-600 absolute text-xl' />
+            <input
+                type='text'
+                placeholder='Ex: Modern...'
+                className='relative bg-transparent focus:outline-none w-[500px] rounded-lg pl-[40px] text-xl'
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
+            />
+            <button className='btn-color py-2 px-6 rounded-lg title-color font-semibold tracking-wider transition-all duration-200 '>
+              Search
+            </button>
+          </form>
         </div>
-        <Link
-          to={'/search'}
-          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
-        >
-          Let's get started...
-        </Link>
+        <div className="order-1 lg:order-2">
+            <img 
+            src={homeImage} 
+            alt="home image"
+            className='w-[500px] h-[600px] object-cover rounded-t-[300px] border-stroke  ' />
+        </div>
+        
       </div>
-      {/* swiper */}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220"><path fill="#FEECE2" fill-opacity="1" d="M0,128L120,138.7C240,149,480,171,720,170.7C960,171,1200,149,1320,138.7L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+      {/* swiper
       <Swiper navigation>
         {
           homeImages.map((image,index) => (
@@ -85,7 +110,35 @@ const Home = () => {
               ></div>
             </SwiperSlide>
           ))}
-      </Swiper>
+      </Swiper> */}
+      
+
+      <div className="primary-section py-[50px] flex flex-col items-center justify-center w-full">    
+        <h2 className='title-color text-4xl font-bold text-center'>How it works?</h2>
+        <div className="flex justify-center items-center container mx-auto flex-wrap mt-[50px]  gap-4">
+          <div className="flex-1 flex flex-col items-center justify-center text-center py-[60px]">
+              <div className="p-5 bg-slate-600 rounded-[20px]">
+                <FaSearch className='text-slate-100 text-xl' />
+              </div>
+            <h2 className='title-color text-xl font-semibold pt-[30px]'>Search for your comfort</h2>
+            <p className='desc-color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quasi veniam cumque modi vitae harum.</p>
+          </div>
+          <div className="flex-1 flex flex-col items-center justify-center text-center py-[60px] px-5">
+            <div className="p-5 bg-slate-600 rounded-[20px]">
+              <FaMoneyBillAlt className='text-slate-100 text-xl' />
+            </div>
+            <h2 className='title-color text-xl font-semibold pt-[30px]'>Instant Valuation</h2>
+            <p className='desc-color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quasi veniam cumque modi vitae harum.</p>
+          </div>
+          <div className="flex-1 flex flex-col items-center justify-center text-center py-[60px]">
+            <div className="p-5 bg-slate-600 rounded-[20px]">
+              <FaRegBuilding className='text-slate-100 text-xl' />
+            </div>
+            <h2 className='title-color text-xl font-semibold pt-[30px]'>Track Property</h2>
+            <p className='desc-color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quasi veniam cumque modi vitae harum.</p>
+          </div>
+        </div>
+      </div>
       {/* listings results for offer, sale, and rent */}
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
         {offerListings && offerListings.length > 0 && (
