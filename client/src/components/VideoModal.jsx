748b27dfcd9327b543c5f6770f49442919467aef
Beni <beninoje@hotmail.com>
2024-03-07 13:14:42 -0500

    added more front end experience on the home page

---
+++ client/src/components/VideoModal.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import videoImg2 from '../images/videoImg.png';
+import videoHouse from '../videos/house_tour.mp4';
+
+const VideoModal = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    useEffect(()=>{
+    const handleClickOutSideModal = (e) =>{
+      if(isModalOpen && !e.target.closest('.modal'))
+      {
+        setIsModalOpen(false);
+      }
+      document.addEventListener('click', handleClickOutSideModal);
+
+      return () =>{
+        document.removeEventListener('click', handleClickOutSideModal);
+      }
+    };
+  },[isModalOpen]);
+  const handleVideo = () =>{
+    setIsModalOpen(true);
+  }
+  return (
+    <div>
+        <div className="absolute bottom-[-20px] right-[-20px]">
+                  <img 
+                    src={videoImg2} 
+                    alt=""
+                    className='w-[300px] h-[300px] rounded-lg relative border-[3px] border-[#FFBE98]' />
+                      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+                      
+                        <button className='cursor-pointer z-10' onClick={handleVideo}>
+                          <svg width="129" height="123" viewBox="0 0 129 123" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M64.5 112.75C94.185 112.75 118.25 89.8043 118.25 61.5C118.25 33.1954 94.185 10.25 64.5 10.25C34.8147 10.25 10.75 33.1954 10.75 61.5C10.75 89.8043 34.8147 112.75 64.5 112.75Z" fill="#333333"/>
+                            <path d="M82.8486 66.9274L57.4776 81.2097C53.3937 83.5088 48.375 80.5163 48.375 75.7824V47.2174C48.375 42.4835 53.3937 39.4913 57.4776 41.7902L82.8486 56.0726C87.0503 58.4383 87.0503 64.5617 82.8486 66.9274Z" fill="#333333"/>
+                          </svg>
+                        </button>
+                        
+                      </div>
+                      {/* <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.3)] opacity-100 z-[9]">
+                      </div> */}
+                </div>
+        {isModalOpen &&(
+                          <div className="fixed modal top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+                            <div className="modal shadow-lg">
+                              {/* Video Player */}
+                              <video controls autoPlay className="w-full rounded-lg">
+                                <source src={videoHouse} type="video/mp4" />
+                              </video>
+                  
+                              {/* Close Button */}
+                              <button className="absolute top-[20px] right-[20px] p-2" onClick={() => setIsModalOpen(false)}>
+                              <svg className='w-[40px] h-[40px]' viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M36.3291 10.2931L29.0251 17.5971C28.9938 17.6284 28.961 17.6571 28.9269 17.6834C28.9002 17.7181 28.871 17.7515 28.8393 17.7833L25.4403 21.1822L26.8539 22.5958L37.7428 11.7069C38.1342 11.3156 38.1342 10.6834 37.7419 10.2922C37.3527 9.90193 36.7192 9.90193 36.3291 10.2931ZM28.2682 24.01L39.1571 13.1211C40.3297 11.9484 40.3297 10.0486 39.1562 8.87798C37.9872 7.70606 36.0847 7.70606 34.9139 8.87979L27.6108 16.1829C27.5791 16.2147 27.5499 16.2481 27.5232 16.2828C27.4891 16.3091 27.4563 16.3378 27.425 16.3691L24.0261 19.768L23.3883 19.1301C23.3784 19.1202 23.3684 19.1107 23.3583 19.1013L13.1527 8.89569C11.9828 7.72286 10.0803 7.72286 8.90954 8.89659C7.74007 10.0691 7.74007 11.9675 8.91044 13.1379L19.7833 24.0108L8.92904 34.8651C8.80498 34.9853 8.63403 35.1885 8.46649 35.4607C7.76939 36.5931 7.76653 37.9264 8.92154 39.0798C10.0972 40.2298 11.4174 40.2314 12.5531 39.5674C12.8256 39.408 13.0302 39.2449 13.1621 39.1163L21.9373 30.3413C22.3278 29.9508 22.3278 29.3176 21.9373 28.9271C21.5467 28.5366 20.9136 28.5366 20.523 28.9271L11.757 37.6931C11.7422 37.7074 11.6609 37.7722 11.5436 37.8408C11.1166 38.0905 10.7697 38.0901 10.351 37.6807C9.91539 37.2458 9.91609 36.921 10.1697 36.5091C10.2407 36.3938 10.3083 36.3134 10.3326 36.2898L24.0261 22.5964L25.4397 24.01L23.4948 25.9549C23.3637 26.086 23.2766 26.2445 23.2335 26.412C23.0117 26.7946 23.0645 27.2928 23.392 27.6203L34.908 39.1363C36.0806 40.3088 37.9797 40.3088 39.1523 39.1363C40.3226 37.9629 40.3226 36.0645 39.1523 34.8941L28.2682 24.01ZM26.8539 25.4242L25.4392 26.839L36.3223 37.7221C36.7137 38.1136 37.3466 38.1136 37.7371 37.723C38.1277 37.3315 38.1277 36.6979 37.738 36.3083L26.8539 25.4242ZM22.6113 21.1828L21.1975 22.5966L10.3247 11.7237C9.93503 11.3341 9.93503 10.7005 10.3256 10.309C10.7148 9.91873 11.3483 9.91873 11.7375 10.309L22.6113 21.1828Z" fill="white"/>
+                              </svg>
+                              </button>
+                            </div>
+                          </div>
+        )}
+    </div>
+  )
+}
+
+export default VideoModal
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,27 @@ import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
 import homeImage from '../images/homeImage.jpg';
+import cottage from '../images/cottage.jpg';
+import townhouse from '../images/townhouse.jpg';
+import hotel from '../images/hotel.jpg';
+import bungalow from '../images/bungalow.jpeg';
+import apartment from '../images/apartment.jpg';
+import comprehensiveImg from '../images/comprehensive.png';
+import profile1 from '../images/profile1.jpeg';
+import profile2 from '../images/profile2.jpg';
+import profile3 from '../images/profile3.jpg';
+import videoImg1 from '../images/videoImg1.png';
+
 import { FaSearch, FaMoneyBillAlt, FaRegBuilding } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import VideoModal from '../components/VideoModal';
 const Home = () => {
   const navigate = useNavigate();
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   SwiperCore.use([Navigation]);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -63,6 +76,8 @@ const Home = () => {
     };
     fetchOfferListings();
   },[])
+  
+  
   return (
     <div>
       {/* top */}
@@ -96,23 +111,6 @@ const Home = () => {
         
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220"><path fill="#FEECE2" fill-opacity="1" d="M0,128L120,138.7C240,149,480,171,720,170.7C960,171,1200,149,1320,138.7L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
-      {/* swiper
-      <Swiper navigation>
-        {
-          homeImages.map((image,index) => (
-            <SwiperSlide key={index}>
-              <div
-                style={{
-                  background: `url(${image}) center no-repeat`,
-                  backgroundSize: 'cover',
-                }}
-                className='h-[500px]'
-              ></div>
-            </SwiperSlide>
-          ))}
-      </Swiper> */}
-      
-
       <div className="primary-section py-[50px] flex flex-col items-center justify-center w-full">    
         <h2 className='title-color text-4xl font-bold text-center'>How it works?</h2>
         <div className="flex justify-center items-center container mx-auto flex-wrap mt-[50px]  gap-4">
@@ -140,6 +138,200 @@ const Home = () => {
         </div>
       </div>
       {/* listings results for offer, sale, and rent */}
+      <div className="flex flex-col justify-center items-center py-[100px]">
+        <div className="py-[60px]">
+          <h2 className='title-color text-4xl font-bold text-center'>Discover Our Featured Listings</h2>
+          <p className='desc-color'>These featured listings contain exclusive real estate opportunities within the city</p>
+        </div>  
+        <div className="flex justify-between container mx-auto">
+          <Swiper 
+            className='swiper-container'
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            slidesPerView={3}
+          >
+            {offerListings.map((listing) => (
+              <SwiperSlide key={listing._id}>
+                <div className="flex justify-center">
+                <ListingItem listing={listing} />
+                </div>
+                
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      
+      {/* Explore House Types */}
+      <div className="py-[100px] primary-section">
+        <div className="container mx-auto flex flex-col justify-center items-center">
+            <div className="text-center py-[60px]">
+              <h2 className='title-color text-4xl font-bold'>Explore House Types</h2>
+              <p className='desc-color'>Explore all the different types of apartments so you can choose the best option for you</p>
+            </div>
+            <div class="grid grid-cols-4 gap-4">
+                <div class="row-span-3 relative">
+                    <img 
+                      src={hotel} 
+                      alt=""
+                      className='h-full w-[300px] object-cover block rounded-lg' />
+                      <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.5)] flex justify-center items-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 z-10">
+                        <p class="text-center title-color text-3xl font-semibold">Hotel</p>
+                      </div>
+                </div>
+                <div class="row-span-2 relative">
+                    <img 
+                      src={bungalow} 
+                      alt=""
+                      className='h-full w-[300px] object-cover block rounded-lg' />
+                      <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.5)] flex justify-center items-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 z-10">
+                        <p class="text-center title-color text-3xl font-semibold">Bungalow</p>
+                      </div>
+                </div>
+                <div class="col-span-2 relative">
+                    <img 
+                      src={townhouse} 
+                      alt=""
+                      className='h-[300px] w-full object-cover block rounded-lg' />
+                      <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.5)] flex justify-center items-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 z-10">
+                        <p class="text-center title-color text-3xl font-semibold">Townhouse</p>
+                      </div>
+                </div>
+                <div class="col-span-2 relative">
+                    <img 
+                      src={apartment} 
+                      alt=""
+                      className='h-[300px] w-full object-cover block rounded-lg' />
+                      <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.5)] flex justify-center items-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 z-10">
+                        <p class="text-center title-color text-3xl font-semibold">Apartment</p>
+                      </div>
+                </div>
+                <div class="col-span-3 relative">
+                    <img 
+                      src={cottage} 
+                      alt=""
+                      className='w-full h-[300px] object-cover block rounded-lg' />
+                      <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.5)] flex justify-center items-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100 z-10">
+                        <p class="text-center title-color text-3xl font-semibold">Cottage</p>
+                      </div>
+                </div>
+            </div>
+        </div>
+          
+      </div>
+
+      {/* Comprehensive Suite */}
+      <div className="py-[100px]">
+          <div className="container mx-auto flex items-center justify-center gap-8">
+            <div className="p-3 relative flex-0">
+              <img 
+                src={comprehensiveImg} 
+                alt=""
+                className='w-full block object-cover rounded-lg' />
+                <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.3)] opacity-100">
+                </div>
+            </div>
+            <div className="w-[700px] h-full flex flex-col items-start justify-start">
+              <h2 className='title-color text-4xl font-bold'>Comprehensive Suite</h2>
+              <p className='desc-color mt-[40px]'>Explore our comprehensive suite of social media marketing services designed to connect your brand with your target audience and achieve your business goals. Our mission is to turn your social media presence into a success story.</p>
+              <div className="flex item-center gap-3 py-3 mt-[40px]">
+                <svg className="w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.0303 10.0303C16.3232 9.73744 16.3232 9.26256 16.0303 8.96967C15.7374 8.67678 15.2626 8.67678 14.9697 8.96967L10.5 13.4393L9.03033 11.9697C8.73744 11.6768 8.26256 11.6768 7.96967 11.9697C7.67678 12.2626 7.67678 12.7374 7.96967 13.0303L9.96967 15.0303C10.2626 15.3232 10.7374 15.3232 11.0303 15.0303L16.0303 10.0303Z" fill="#FFBE98"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="#FFBE98"></path> </g></svg>
+                <p className='desc-color'>Developing and executing data-driven strategies</p>
+              </div>
+              <div className="flex item-center gap-3 py-3">
+                <svg className="w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.0303 10.0303C16.3232 9.73744 16.3232 9.26256 16.0303 8.96967C15.7374 8.67678 15.2626 8.67678 14.9697 8.96967L10.5 13.4393L9.03033 11.9697C8.73744 11.6768 8.26256 11.6768 7.96967 11.9697C7.67678 12.2626 7.67678 12.7374 7.96967 13.0303L9.96967 15.0303C10.2626 15.3232 10.7374 15.3232 11.0303 15.0303L16.0303 10.0303Z" fill="#FFBE98"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="#FFBE98"></path> </g></svg>
+                <p className='desc-color'>Take your brand to new heights by creating compelling content</p>
+              </div>
+              <div className="flex item-center gap-3 py-3">
+                <svg className="w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.0303 10.0303C16.3232 9.73744 16.3232 9.26256 16.0303 8.96967C15.7374 8.67678 15.2626 8.67678 14.9697 8.96967L10.5 13.4393L9.03033 11.9697C8.73744 11.6768 8.26256 11.6768 7.96967 11.9697C7.67678 12.2626 7.67678 12.7374 7.96967 13.0303L9.96967 15.0303C10.2626 15.3232 10.7374 15.3232 11.0303 15.0303L16.0303 10.0303Z" fill="#FFBE98"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="#FFBE98"></path> </g></svg>
+                <p className='desc-color'>Designed to connect your brand with your target audience</p>
+              </div>
+              <div className="mt-[70px]">
+                <a href="/about" className='btn-color rounded-3xl px-7 py-2 title-color font-semibold ' >
+                  About Us
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
+      {/* Our Agents */}
+      <div className="py-[100px] primary-section">
+        <div className="container mx-auto flex flex-col justify-center items-center">
+            <h2 className='title-color text-4xl font-bold'>Meet Our Best Agents</h2>
+            <p className='desc-color'>Meet our team of professional agents, who will help you find your dream house</p>
+            <div className="flex justify-evenly items-center  pt-[50px]  w-full gap-3">
+              <div className="flex flex-col justify-center items-center gap-2">
+                <img 
+                  src={profile1} 
+                  alt=""
+                  className='w-[175px] h-[175px] object-cover rounded-full border-2 border-[#bebebe]' />
+                <h3 className='font-bold title-color text-xl'>John Doe</h3>
+                <span className='desc-color'>Real Trader</span>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <img 
+                  src={profile2} 
+                  alt=""
+                  className='w-[175px] h-[175px] object-cover rounded-full border-2 border-[#bebebe]' />
+                <h3 className='font-bold title-color text-xl'>John Doe</h3>
+                <span className='desc-color'>Real Trader</span>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <img 
+                  src={profile3} 
+                  alt=""
+                  className='w-[175px] h-[175px] object-cover rounded-full border-2 border-[#bebebe]' />
+                <h3 className='font-bold title-color text-xl'>John Doe</h3>
+                <span className='desc-color'>Real Trader</span>
+              </div>
+            </div>
+        </div>     
+      </div>
+      {/* Our Agents */}
+      <div className="py-[100px]">
+          <div className="container mx-auto flex items-center justify-center gap-8">
+            <div className="relative flex-0 order-1">
+              <img 
+                src={videoImg1} 
+                alt=""
+                className='w-full block object-cover rounded-lg relative' />
+                <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-[rgba(255,190,152,0.3)] opacity-100">
+                </div>
+                <VideoModal/>
+                
+            </div>
+            <div className="w-[700px] h-full flex flex-col items-start justify-start">
+              <h2 className='title-color text-4xl font-bold'>Comprehensive Suite</h2>
+              <p className='desc-color mt-[40px]'>Explore our comprehensive suite of social media marketing services designed to connect your brand with your target audience and achieve your business goals. Our mission is to turn your social media presence into a success story.</p>
+              <div className="flex item-center gap-3 py-3 mt-[40px]">
+                <p className='desc-color'>Developing and executing data-driven strategies</p>
+              </div>
+              <div className="flex item-center gap-3 py-3">
+                <p className='desc-color'>Take your brand to new heights by creating compelling content</p>
+              </div>
+              <div className="flex item-center gap-3 py-3">
+                <p className='desc-color'>Designed to connect your brand with your target audience</p>
+              </div>
+              <div className="mt-[70px]">
+                <a href="/about" className='btn-color rounded-3xl px-7 py-2 title-color font-semibold ' >
+                  About Us
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
         {offerListings && offerListings.length > 0 && (
           <div className="">
